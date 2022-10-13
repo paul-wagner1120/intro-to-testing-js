98e2a6f62ed7d8076fc151a9ab0c20c57f05c2df
Paul Wagner <p.wagner1120@icloud.com>
2022-10-13 13:17:52 -0500

    adding exercise 6, refactoring, on intro-to-testing

--- code.js
+++ code.js
@@ -4,14 +4,10 @@ function helloWorld() {
 }
 
 function sayHello(input) {
-    if (input === "Alex") {
-        return "Hello, Alex!";
-    } else if (input === "Pat") {
-        return "Hello, Pat!";
-    } else {
-        return "Hello, Jane!";
-    }
+    return "Hello, " + input + "!";
 }
+
+
 
 
 
