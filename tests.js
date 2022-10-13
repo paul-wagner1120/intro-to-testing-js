8d6851afb6046f99e9ef56bb52e718c360a31fd8
Paul Wagner <p.wagner1120@icloud.com>
2022-10-13 11:23:37 -0500

    adding exercise 2 from intro-to-testing

--- tests.js
+++ tests.js
@@ -19,9 +19,9 @@ describe('sayHello', function() {
     it('should be a defined function', function() {
         expect(typeof sayHello).toBe('function');
     });
-    // it('should return a string when called', function() {
-    //     expect(typeof helloWorld()).toBe("string");
-    // });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
     // it('should return the string "Hello, World!" when executed', function() {
     //     expect(helloWorld()).toBe("Hello, World!");
     // });
